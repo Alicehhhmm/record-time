d3825611e837bb3ff12813cde21b0d77a1061a68
Alicehhhmm <a007aplum@gmail.com>
2025-01-22 16:21:48 +0800

    init nextjs

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Open_Sans } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/provider/theme-provider'
+
+const font = Open_Sans({
+    variable: '--font-open-sans',
+    subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+    title: 'Record Time Application',
+    description: 'Record Time app',
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <html lang='en' suppressHydrationWarning>
+            <body className={cn(font.className, `antialiased`, `bg-white dark:bg-[#313333]`)}>
+                <ThemeProvider attribute='class' defaultTheme='light' enableSystem storageKey='discord-theme' disableTransitionOnChange>
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    )
+}
